d4bcabce66240a088f579fed0e48efef1ba55b45
𝚴 𝐎 𝐁 𝚰 𝐓 𝚲 <nobitaxmusicbot@gmail.com>
2025-07-25 15:51:59 +0530

    Update play.py

--- NOBITA_MUSIC/utils/inline/play.py
+++ NOBITA_MUSIC/utils/inline/play.py
@@ -79,7 +79,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
                 text="𝖮ᴡɴᴇʀ 🥀", url="https://t.me/ll_NOBITA_DEFAULTERS_ll",
             ),
             InlineKeyboardButton(
-                text="𝖲ᴜᴘᴘᴏʀᴛ 🥀", url="https://t.me/+wPjAlUcObehiZDM1",
+                text="𝖲ᴜᴘᴘᴏʀᴛ 🥀", url="https://t.me/+WLTHgUAvkYVmNTg9",
             )
         ],
          [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
